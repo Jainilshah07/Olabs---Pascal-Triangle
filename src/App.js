28a9e215e8e028a4cb56581ed59c33ed0a696096
Jainil <shahjainilb@gmail.com>
2023-10-26 12:17:53 +0530

    start page added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@
 import AdditionState from "./components/AdditionState"
 import SymmetricState from "./components/SymmetricState";
 import BinomialTriangle from "./components/BinomialTriangle";
-import Trial from "./components/Trial";
+import { Route, Routes,  } from "react-router-dom";
+import StartPage from "./components/introduction/StartPage";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App font-mono">
+      <Routes>
+        <Route path="/" element={<StartPage />} ></Route>
+        <Route path="/level-1" element={<AdditionState />} ></Route>
+        <Route path="/level-2" element={<SymmetricState />} ></Route>
+        <Route path="/level-3" element={<BinomialTriangle/>} ></Route>
+        </Routes>
       {/* <AdditionState /> */}
       {/* <SymmetricState /> */}
-      <BinomialTriangle/>
+      {/* <BinomialTriangle/> */}
     </div>
   );
 }
---
+++ src/components/introduction/StartPage.js
@@ -0,0 +1,30 @@
+import React from "react";
+import background from "../../assets/mathBackground.svg";
+import { Link } from "react-router-dom";
+
+const StartPage = () => {
+  return (
+    <div className="h-screen w-screen">
+    <div
+      className="bg-cover bg-center h-full"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: 'cover',
+      }}
+    >
+      <p className="text-slate-300 font-bold text-2xl text-center pt-72">
+        Deriving Binomial Coefficients using Pascal's Triangle
+      </p>
+      <div className="flex flex-col items-center mt-4">
+      <Link to={'/level-1'}><button className="bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded">
+        Start
+      </button></Link> 
+      </div>
+      
+    </div>
+    </div>
+  );
+};
+
+export default StartPage;
